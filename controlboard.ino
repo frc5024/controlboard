a32f93e6d8afe054245bbca7e5e0123219049a68
Evan Pratten <ewpratten@gmail.com>
2019-12-29 10:57:25 -0500

    Added program header

--- controlboard.ino
+++ controlboard.ino
@@ -1,3 +1,33 @@
+/**
+ * ############################## License Info ################################
+ * This file is part of ControlBoard <https://github.com/frc5024/controlboard>.
+ * Copyright (c) 2020 Raider Robotics.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * ############################## Program Info ################################
+ *
+ * Team 5024's Arduino-based DriverStation control board source code.
+ *
+ * This software is designed to run on an Arduino Leonardo. It will register
+ * itself with a host computer as a standard HID joystick. This joystick can be
+ * read by FRC DriverStation software, and can be used in FRC robot code with
+ * the help of Lib5K's frc.lib5k.interface.controlboard package.
+ */
+
+#include "Joystick.h"
+
 void setup() {}
 
 void loop() {}
